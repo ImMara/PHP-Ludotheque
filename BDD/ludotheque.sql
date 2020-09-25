de3b97d3318be080f98e6de3e250ad0638c4e2dd
maracax <alaneraerts@live.be>
2020-09-25 21:25:33 +0200

    'bdd-update'

--- BDD/ludotheque.sql
+++ BDD/ludotheque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 21 sep. 2020 à 12:27
+-- Généré le :  ven. 25 sep. 2020 à 19:23
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `jeux` (
   `type` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_support` (`id_support`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `jeux`
@@ -47,7 +47,11 @@ CREATE TABLE IF NOT EXISTS `jeux` (
 
 INSERT INTO `jeux` (`id`, `nom`, `id_support`, `pochette`, `description`, `editeur`, `type`) VALUES
 (1, 'League of Legends', 1, 'lol.jpg\r\n', 'Inspiré du mod DotA (Defense of the Ancients) de Warcraft III, League of Legends est un MOBA, une arène de bataille en ligne multijoueur. Dans le mode classique, deux équipes de cinq joueurs s\'affrontent dans des parties qui durent en moyenne entre 40 minutes et dont l\'objectif est de détruire la base ennemie. Évoluant dans un univers heroic-fantasy, chaque joueur incarne un champion différent, aux capacités uniques, qu\'il choisit à chaque début de partie. Des modes aléatoires sont également présents, ainsi que des événements saisonniers qui apportent un souffle de nouveauté.', 'Riot Games ', 'Stratégie,MOBA '),
-(2, 'Red Dead Redemption', 2, 'red.jpg', 'Suite du précédent volet multi récompensé, Red Dead Redemption II nous permet de nous replonger sur PS4 dans une ambiance western synonyme de vastes espaces sauvages et de villes malfamées. L\'histoire se déroule en 1899, avant le premier Red Dead Redemption, au moment où Arthur Morgan doit fuir avec sa bande à la suite d\'un braquage raté.', 'Rockstar Games ', 'Action,TPS,Aventure ');
+(2, 'Red Dead Redemption', 2, 'red.jpg', 'Suite du précédent volet multi récompensé, Red Dead Redemption II nous permet de nous replonger sur PS4 dans une ambiance western synonyme de vastes espaces sauvages et de villes malfamées. L\'histoire se déroule en 1899, avant le premier Red Dead Redemption, au moment où Arthur Morgan doit fuir avec sa bande à la suite d\'un braquage raté.', 'Rockstar Games ', 'Action,TPS,Aventure '),
+(3, 'Tekken 7', 1, 't7.jpg', 'Tekken 7 est le septième épisode de la série de jeux de combat éponyme. Cet épisode comprend d\'anciens personnages de la série, mais également de nouvelles têtes telles que Katarina, Claudio et d\'autres encore...', 'Bandai Namco ', 'Combat '),
+(4, 'Valorant', 1, 'val.jpg', 'Valorant, anciennement Project A, est un FPS tactique dans la veine de Counter Strike et de Team Fortress. Le jeu développé et édité par Riot Games proposera de nombreux personnages aux capacités uniques, différents modes de jeu et promet déjà des serveurs sans failles et un système anti-triche à la pointe de la technologie.', 'Riot Games ', 'FPS Tactique '),
+(5, 'Legends of Runeterra', 1, 'rune.jpg', 'Legends of Runeterra (LoR) est un jeu de cartes de stratégie gratuit se déroulant dans le monde de LoL. LoR inclut des cartes inspirées de champions mythiques de LoL, ainsi que de nouveaux alliés et personnages issus des régions de Runeterra, possédant chacun leur propre style et atouts stratégiques.', 'Riot Games', 'Cartes'),
+(6, 'Space Engineers', 1, 'space.jpg', 'La survie dans l\'espace ou sur des planètes étrangères est au cœur de Space Engineers qui vous propose une large gamme de ressources à récolter et d\'objets à construire, tout ça dans le but de survivre le plus longtemps possible au milieu du grand vide intersidéral.', 'Keen Software ', 'Gestion Création Simulation Survie ');
 
 -- --------------------------------------------------------
 
